efb194e3bed6b3ff3cb544956950af4c7e5c7c56
Brenner Lopes <brennerlopes94@gmail.com>
2016-08-30 00:39:39 -0300

    Inserção do ProjetoPoster.Web

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FATEA.ProjetoPoster.Web.Startup))]
+namespace FATEA.ProjetoPoster.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
