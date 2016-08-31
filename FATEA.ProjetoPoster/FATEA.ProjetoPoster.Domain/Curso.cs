fab77ae5081b0e06071891b0a6aea83cf4487f71
Marylene <marylene.guedes@gmail.com>
2016-08-31 20:25:02 -0300

    Inclusão das classes Aluno e Curso

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Domain/Curso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATEA.ProjetoPoster.Domain
+{
+    class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Area { get; set; }
+    }
+}
