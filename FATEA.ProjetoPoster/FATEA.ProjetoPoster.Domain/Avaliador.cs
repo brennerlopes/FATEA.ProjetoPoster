64e3847110a032ededecf2035a3a660a7ee2da43
Brenner Lopes <brennerlopes94@gmail.com>
2016-08-31 22:10:37 -0300

    Inserção da Classe Avaliador

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Domain/Avaliador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATEA.ProjetoPoster.Domain
+{
+   public class Avaliador{
+        public string Area { get; set; }
+
+        public void visualizarPosteres() { }
+        public void atribuiNota() { }
+    }
+}
