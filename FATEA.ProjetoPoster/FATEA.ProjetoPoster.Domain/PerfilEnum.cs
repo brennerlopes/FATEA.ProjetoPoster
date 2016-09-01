fd4e094c74db20eec01c75e73f9725ed1dc39790
Felipe Guimares <f3lpz@hotmail.com>
2016-08-31 21:02:35 -0300

    Add usuario

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Domain/PerfilEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATEA.ProjetoPoster.Domain
+{
+    public enum PerfilEnum
+    {
+        Aluno = 0,
+        Avaliador = 1,
+        Administrador = 2
+        
+    }
+}
--- FATEA.ProjetoPoster/FATEA.ProjetoPoster.Domain/Usuario.cs
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Domain/Usuario.cs
@@ -6,19 +6,15 @@ using System.Threading.Tasks;
 
 namespace FATEA.ProjetoPoster.Domain
 {
-    public class Usuario
+    class Usuario
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
-        public int Rg { get; set; }
-        public int Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Cpf { get; set; }
         public string Login { get; set; }
         public string Senha { get; set; }
-        public Enum Perfil { get; set; }
-
-        public void fazerLogin() { }
-                
-        public void alterarSenha() { }
+        public PerfilEnum Perfil { get; set; }
 
     }
 }
