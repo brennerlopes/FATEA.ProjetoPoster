70c39c952102b05569b128510058b5c1a7b2f558
Brenner Lopes <brennerlopes94@gmail.com>
2016-09-12 21:24:46 -0300

    Refazendo o processo pois houve um conflito

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.DataAccess.Entity/Configurations/CursoTypeConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using FATEA.DataAccess.Entity.Common;
+using FATEA.ProjetoPoster.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATEA.ProjetoPoster.DataAccess.Entity.Configurations
+{
+    class CursoTypeConfiguration : FATEATypeConfiguration<Curso>
+
+    {
+        public override void ConfigureFields()
+        {
+            Property(c => c.Id)
+                   .HasColumnName("CUR_ID")
+                   .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+                   .IsRequired();
+
+            Property(c => c.Nome)
+                .HasColumnName("CUR_NOME")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            Property(c => c.Area)
+                .HasColumnName("CUR_AREA")
+                .HasMaxLength(50)
+                .IsRequired();              
+        }
+
+        public override void ConfigureForeignKeys()
+        {
+          
+        }
+
+        public override void ConfigureOthers()
+        {
+           
+        }
+
+        public override void ConfigurePrimaryKeys()
+        {
+            HasKey(pk => pk.Id);
+        }
+
+        public override void ConfigureTableName()
+        {
+            ToTable("CUR_CURSOS");
+        }
+    }
+}
