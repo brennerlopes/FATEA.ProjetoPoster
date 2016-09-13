1aa4eac58aa8482785e2d7a99fdbf5c461dff129
Brenner Lopes <brennerlopes94@gmail.com>
2016-09-12 21:49:07 -0300

    Inserção da Classe CursoRepository no projeto Repository

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Repository/CursoRepository.cs
@@ -0,0 +1,19 @@
+﻿using FATEA.ProjetoPoster.Domain;
+using FATEA.ProjetoPoster.Repository.Common.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATEA.ProjetoPoster.Repository
+{
+    public class CursoRepository : EntityCrudRepository<Curso, long>
+    {
+        public EventoRepository(ProjetoPosterDbContext contexto)
+            : base(contexto)
+        {
+
+        }
+    }
+}
