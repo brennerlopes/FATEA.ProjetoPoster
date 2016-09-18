32cd6af9eba64b457c9126acc88adb1cf2f4ee86
Brenner Lopes <brennerlopes94@gmail.com>
2016-09-18 15:47:44 -0300

    Criação da classe CursoController na camada Web

---
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Web/Controllers/CursosController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using FATEA.ProjetoPoster.DataAccess.Entity.Context;
+using FATEA.ProjetoPoster.Domain;
+using FATEA.ProjetoPoster.Repository;
+using FATEA.ProjetoPoster.Repository.Common;
+using FATEA.ProjetoPoster.Web.ViewModels.Curso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FATEA.ProjetoPoster.Web.Controllers
+{
+    public class CursosController : Controller 
+    {
+        private ICrudRepositorio<Curso, long> _repositorio = new CursoRepository(new ProjetoPosterDbContext());
+
+        public ActionResult Index()
+        {
+            List<CursoIndexViewModel> viewModels = new List<CursoIndexViewModel>();
+            List<Curso> curso = _repositorio.Select();
+            viewModels = Mapper.Map<List<Curso>, List<CursoIndexViewModel>>(curso);
+            return View(viewModels);
+
+        }
+    }
+}
