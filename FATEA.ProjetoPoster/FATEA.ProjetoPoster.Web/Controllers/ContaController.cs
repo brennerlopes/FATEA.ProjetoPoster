59a66c982ebad28b55beed8f6510d141763c8c18
Brenner Lopes <brennerlopes94@gmail.com>
2016-09-26 09:19:18 -0300

    Alteração na classe ContaController

--- FATEA.ProjetoPoster/FATEA.ProjetoPoster.Web/Controllers/ContaController.cs
+++ FATEA.ProjetoPoster/FATEA.ProjetoPoster.Web/Controllers/ContaController.cs
@@ -1,6 +1,5 @@
-﻿using FATEA.ProjetoPoster.DataAccess.Entity.Context;
+﻿using FATEA.ProjetoPoster.Web.ViewModels.Usuario;
 using FATEA.ProjetoPoster.Web.Identity;
-using FATEA.ProjetoPoster.Web.ViewModels.Usuario;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
@@ -12,17 +11,17 @@ using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
-
 namespace FATEA.ProjetoPoster.Web.Controllers
+
 {
+
     [AllowAnonymous]
     public class ContaController : Controller
     {
-
         public ActionResult CriarUsuario()
         {
             RoleStore<IdentityRole> roleStore
-                = new RoleStore<IdentityRole>(new ProjetoPosterDbContext());
+                = new RoleStore<IdentityRole>(new ProjetoPosterIdentityDbContext());
             RoleManager<IdentityRole> roleManager
                 = new RoleManager<IdentityRole>(roleStore);
             ViewBag.Roles = new SelectList(roleManager.Roles, "ID", "Name");
@@ -31,8 +30,7 @@ namespace FATEA.ProjetoPoster.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
-        public ActionResult CriarUsuario(UsuarioEditViewModel viewModel)
+        public ActionResult CreateUser(UsuarioEditViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
@@ -47,13 +45,18 @@ namespace FATEA.ProjetoPoster.Web.Controllers
                 };
 
                 IdentityResult result = userManager.Create(user, viewModel.Senha);
-
                 if (result.Succeeded)
                 {
-
+                    RoleStore<IdentityRole> roleStore
+                        = new RoleStore<IdentityRole>(new ProjetoPosterIdentityDbContext());
+                    RoleManager<IdentityRole> roleManager
+                        = new RoleManager<IdentityRole>(roleStore);
+                    IdentityRole selectRole
+                        = roleManager.Roles.Single(s => s.Id == viewModel.RoleId);
+                    IdentityUser insertedUser = userManager.Find(viewModel.NomeUsuario, viewModel.Senha);
+                    userManager.AddToRole(insertedUser.Id, selectRole.Name);
                     return RedirectToAction("Index", "Home");
                 }
-
                 else
                 {
                     ModelState.AddModelError("identity_error", result.Errors.First());
@@ -62,7 +65,6 @@ namespace FATEA.ProjetoPoster.Web.Controllers
             }
             return View(viewModel);
         }
-
         public ActionResult Login()
         {
             return View();
@@ -75,45 +77,32 @@ namespace FATEA.ProjetoPoster.Web.Controllers
             if (ModelState.IsValid)
             {
                 UserStore<IdentityUser> userStore
-
                     = new UserStore<IdentityUser>(new ProjetoPosterIdentityDbContext());
                 UserManager<IdentityUser> userManager
                     = new UserManager<IdentityUser>(userStore);
 
-                IdentityUser user = new IdentityUser
-                {
-                    UserName = viewModel.NomeUsuario,
-                    Email = viewModel.NomeUsuario
+                IdentityUser user = userManager.Find(viewModel.NomeUsuario, viewModel.Senha);
 
-                };
-
-                IdentityResult result = userManager.Create(user, viewModel.Senha);
-                if (result.Succeeded)
+                if (user != null)
                 {
-                    RoleStore<IdentityRole> roleStore
-                        = new RoleStore<IdentityRole>(new ProjetoPosterDbContext());
-                    RoleManager<IdentityRole> roleManager
-                        = new RoleManager<IdentityRole>(roleStore);
-                    IdentityRole selectRole
-                        = roleManager.Roles.Single(s => s.Id == viewModel.RoleId);
-                    IdentityUser insertedUser = userManager.Find(viewModel.NomeUsuario, viewModel.Senha);
-                    userManager.AddToRole(insertedUser.Id, selectRole.Name);
+                    IAuthenticationManager authManager
+                        = HttpContext.GetOwinContext().Authentication;
+                    ClaimsIdentity userIdentity =
+                        userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    authManager.SignIn(userIdentity);
                     return RedirectToAction("Index", "Home");
                 }
-
-
                 else
                 {
                     ModelState.AddModelError("identity_error", "Nome de usuário ou senha incorreta");
                     return View(viewModel);
                 }
 
-
-
             }
-
             return View(viewModel);
         }
+
         [Authorize]
         public ActionResult Logout()
         {
@@ -122,10 +111,7 @@ namespace FATEA.ProjetoPoster.Web.Controllers
 
             authManager.SignOut();
             return RedirectToAction("Login", "Conta");
-
         }
 
     }
-
 }
-
